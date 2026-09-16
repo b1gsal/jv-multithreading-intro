920b712308f044fed6b2c9cbff798ebb54041b65
Denya Basarab <denya2007basarab@gmail.com>
2026-09-16 19:42:05 +0300

    feat: implement two threads

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread threadByExtending = new MyThread();
+        MyRunnable runnable = new MyRunnable();
+        Thread threadByImplementing = new Thread(runnable);
+        threadByImplementing.setDaemon(true);
+        threadByExtending.start();
+        threadByImplementing.start();
     }
 }
